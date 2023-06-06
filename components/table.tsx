88e3f746e14afbcd2c6efd2e0eafc88cc68ae8c6
iamgenechua <iamgenechua@gmail.com>
2023-06-07 02:54:56 +0800

    add chart component and logic

--- components/table.tsx
+++ components/table.tsx
@@ -5,7 +5,7 @@ import RefreshButton from './refresh-button'
 
 export default async function Table() {
   const startTime = Date.now()
-  const users = await prisma.users.findMany()
+  // const users = await prisma.users.findMany()
   const duration = Date.now() - startTime
 
   return (
@@ -14,33 +14,33 @@ export default async function Table() {
         <div className="space-y-1">
           <h2 className="text-xl font-semibold">Recent Users</h2>
           <p className="text-sm text-gray-500">
-            Fetched {users.length} users in {duration}ms
+            {/*Fetched {users.length} users in {duration}ms*/}
           </p>
         </div>
         <RefreshButton />
       </div>
       <div className="divide-y divide-gray-900/5">
-        {users.map((user) => (
-          <div
-            key={user.name}
-            className="flex items-center justify-between py-3"
-          >
-            <div className="flex items-center space-x-4">
-              <Image
-                src={user.image}
-                alt={user.name}
-                width={48}
-                height={48}
-                className="rounded-full ring-1 ring-gray-900/5"
-              />
-              <div className="space-y-1">
-                <p className="font-medium leading-none">{user.name}</p>
-                <p className="text-sm text-gray-500">{user.email}</p>
-              </div>
-            </div>
-            <p className="text-sm text-gray-500">{timeAgo(user.createdAt)}</p>
-          </div>
-        ))}
+        {/*{users.map((user) => (*/}
+        {/*  <div*/}
+        {/*    key={user.name}*/}
+        {/*    className="flex items-center justify-between py-3"*/}
+        {/*  >*/}
+        {/*    <div className="flex items-center space-x-4">*/}
+        {/*      <Image*/}
+        {/*        src={user.image}*/}
+        {/*        alt={user.name}*/}
+        {/*        width={48}*/}
+        {/*        height={48}*/}
+        {/*        className="rounded-full ring-1 ring-gray-900/5"*/}
+        {/*      />*/}
+        {/*      <div className="space-y-1">*/}
+        {/*        <p className="font-medium leading-none">{user.name}</p>*/}
+        {/*        <p className="text-sm text-gray-500">{user.email}</p>*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*    <p className="text-sm text-gray-500">{timeAgo(user.createdAt)}</p>*/}
+        {/*  </div>*/}
+        {/*))}*/}
       </div>
     </div>
   )
