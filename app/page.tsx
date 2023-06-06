7d84b9f979161d99db3c9f71b6b94f2d8820bcd0
Chu You Chia <chuyouchia@Chus-MacBook-Air.local>
2023-06-06 23:00:35 +0800

    add table and pagination

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export const dynamic = 'force-dynamic'
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
+    <main className="relative flex min-h-screen flex-col items-center justify-center w-full">
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
         H1B1 Visa Data
       </h1>
--- components/table.tsx
+++ components/table.tsx
@@ -1,25 +1,270 @@
+'use client'
 import prisma from '@/lib/prisma'
 import { timeAgo } from '@/lib/utils'
+import { users } from '@prisma/client'
 import Image from 'next/image'
 import RefreshButton from './refresh-button'
+import { Table } from 'antd';
 
-export default async function Table() {
+const dataSource = [
+  {
+    key: '1',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '2',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '3',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '4',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '5',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '6',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '7',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '8',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '9',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '10',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '11',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '12',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '13',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '14',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '15',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '16',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '17',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '18',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '19',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '20',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '21',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '22',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '23',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '24',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '25',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '26',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+  {
+    key: '27',
+    status: 'Withdrawn',
+    age: 32,
+    baseSalary: '100000',
+    startDate: '12/31/23',
+    lcaCaseNumber: 'xxx'
+  },
+  {
+    key: '28',
+    status: 'Approved',
+    age: 42,
+    baseSalary: '100000',
+    startDate: '12/34/1234'
+  },
+];
+
+const columns = [
+  {
+    title: 'CASE number',
+    dataIndex: 'lcaCaseNumber',
+    key: 'lcaCaseNumber',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Base Salary (USD)',
+    dataIndex: 'baseSalary',
+    key: 'baseSalary',
+  },
+  {
+    title: 'Start Date',
+    dataIndex: 'startDate',
+    key: 'startDate',
+  },
+];
+
+export default async function TableWrapper() {
   const startTime = Date.now()
-  const users = await prisma.users.findMany()
+  // const users = await prisma.users.findMany()
+  const users: users[] = [];
   const duration = Date.now() - startTime
 
   return (
-    <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
+    <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-5xl mx-auto w-full">
       <div className="flex justify-between items-center mb-4">
         <div className="space-y-1">
           <h2 className="text-xl font-semibold">Recent Users</h2>
-          <p className="text-sm text-gray-500">
-            Fetched {users.length} users in {duration}ms
-          </p>
         </div>
         <RefreshButton />
       </div>
       <div className="divide-y divide-gray-900/5">
+          <div>
+            <Table dataSource={dataSource} columns={columns} />
+          </div>
         {users.map((user) => (
           <div
             key={user.name}
