d6a3aed1b3d82905723181a98e678ee1e690ea46
Jeremy <jtkw@Jeremys-Air.attlocal.net>
2023-09-02 21:38:25 -0700

    FE change

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -3,15 +3,18 @@
 import { Suspense, useEffect, useState } from 'react'
 import TablePlaceholder from '@/components/table-placeholder'
 import { LCATable } from '@/components/lca-table'
-import { Spin } from 'antd';
+import { Layout, Menu, Spin, Typography } from 'antd';
 import { LCAData } from '@/pages/api/get_lca_data';
 import { ConfigProvider, theme } from "antd";
+import Link from 'next/link';
 
 // Prisma does not support Edge without the Data Proxy currently
 // export const runtime = 'edge'
 export const preferredRegion = 'home'
 export const dynamic = 'force-dynamic'
 
+const { Content, Header, Footer } = Layout;
+const { Title } = Typography;
 
 export default function Home() {
   //call prisma in a useEffect hook and store its state to a useState hook
@@ -32,17 +35,43 @@ export default function Home() {
       })
   }, [])
 
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <ConfigProvider theme={{algorithm: defaultAlgorithm}}>
-      <main className="relative flex min-h-screen flex-col items-center justify-center">
-        <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-          H1B1 Visa Data
-        </h1>
-        <Suspense fallback={<TablePlaceholder />}>
-          {loading && <Spin size='large' />}
-          {!loading && <LCATable lcaData={lcaData} />}
-        </Suspense>
-      </main>
+    <ConfigProvider theme={{algorithm: darkAlgorithm}}>
+        <Layout style={{height:"100vh"}}>
+          <Content style={{ margin: '32px 24px 0' }}>
+            <Title style={{textAlign: "left", margin: "24px 32px"}}>H1B1 Visa</Title>
+            <div style={{ padding: 24, minHeight: 360, background: "black"}}>
+                {loading && <Spin size='large' />}
+                {!loading && <LCATable lcaData={lcaData} />}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Built by{' '}
+            <Link
+              href="https://github.com/koonweee"
+              className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+            >
+              Jeremy Tan
+            </Link>
+            ,{' '}
+            <Link
+              href="https://github.com/chuyouchia"
+              className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+            >
+              Jacob Chia
+            </Link>{' '}
+            and{' '}
+            <Link
+              href="https://github.com/iamgenechua"
+              className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+            >
+              Gene Chua
+            </Link>
+          </Footer>
+        </Layout>
     </ConfigProvider>
   )
 }
--- components/lca-table/index.tsx
+++ components/lca-table/index.tsx
@@ -2,7 +2,7 @@
 import { formatColumnTypesWithFilters, LCA_TABLE_COLUMNS } from "@/components/lca-table/column-type";
 import { LCADataToTableDataSource } from "@/components/lca-table/formatters";
 import { LCAData } from "@/pages/api/get_lca_data";
-import { Table } from "antd";
+import { Layout, Table } from "antd";
 
 interface Props {
   lcaData: LCAData[];
@@ -16,17 +16,8 @@ export function LCATable({ lcaData }: Props) {
   // format table columns with filters based on pulled data
   const formattedColumnsWithFilter = formatColumnTypesWithFilters(LCA_TABLE_COLUMNS, lcaDataSource);
   return (
-    <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-auto mx-auto">
-      <div className="flex justify-between items-center mb-4">
-        <div className="space-y-1">
-          <h2 className="text-xl font-semibold">Labour Condition Applications (LCA)</h2>
-        </div>
-      </div>
-      <div className="divide-y divide-gray-900/5">
-          <div>
-            <Table dataSource={lcaDataSource} columns={formattedColumnsWithFilter} />
-          </div>
-      </div>
-    </div>
+    <Layout>
+      <Table dataSource={lcaDataSource} columns={formattedColumnsWithFilter} />
+    </Layout>
   )
 }
