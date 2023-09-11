2009b2abea460b7158ccad69d5a2320277c44ff7
Chu You Chia <chuyouchia@chus-air.lan>
2023-09-10 18:21:09 -0700

    make filters searchable

--- src/components/lca-table/column-type.tsx
+++ src/components/lca-table/column-type.tsx
@@ -24,7 +24,6 @@ export const BASE_LCA_TABLE_COLUMNS: ColumnsType<LcaTableData> = [
     dataIndex: 'caseStatus',
     key: 'caseStatus',
     filters: [],
-    filterMode: 'tree',
     filterSearch: true,
     onFilter: (value, record) => record.caseStatus.startsWith(value as string), //type assertion because we know the values have to be strings
   },
@@ -33,7 +32,6 @@ export const BASE_LCA_TABLE_COLUMNS: ColumnsType<LcaTableData> = [
     dataIndex: 'jobTitle',
     key: 'jobTitle',
     filters: [],
-    filterMode: 'tree',
     filterSearch: true,
     onFilter: (value, record) => record.jobTitle.startsWith(value as string), //type assertion because we know the values have to be strings
   },
@@ -48,7 +46,6 @@ export const BASE_LCA_TABLE_COLUMNS: ColumnsType<LcaTableData> = [
     dataIndex: 'employerName',
     key: 'employerName',
     filters: [],
-    filterMode: 'tree',
     filterSearch: true,
     onFilter: (value, record) => record.employerName.startsWith(value as string), //type assertion because we know the values have to be strings
   },
@@ -56,7 +53,6 @@ export const BASE_LCA_TABLE_COLUMNS: ColumnsType<LcaTableData> = [
     title: 'Employer City',
     dataIndex: 'employerCity',
     key: 'employerCity',
-    filterMode: 'tree',
     filterSearch: true,
     onFilter: (value, record) => record.employerCity.startsWith(value as string), //type assertion because we know the values have to be strings
   },
@@ -64,7 +60,6 @@ export const BASE_LCA_TABLE_COLUMNS: ColumnsType<LcaTableData> = [
     title: 'Employer State',
     dataIndex: 'employerState',
     key: 'employerState',
-    filterMode: 'tree',
     filterSearch: true,
     onFilter: (value, record) => record.employerState.startsWith(value as string), //type assertion because we know the values have to be strings
   },
@@ -74,13 +69,13 @@ export const BASE_LCA_TABLE_COLUMNS: ColumnsType<LcaTableData> = [
 export const formatColumnTypesWithFilters = (columns: ColumnsType<LcaTableData>, data: LcaTableData[], getColumnSearchProps:(dataIndex: DataIndex) => ColumnType<LcaTableData>): ColumnsType<LcaTableData> => {
    //function that takes all values for specific attribute in lca data and returns an array of unique values
     const getUniqueValuesFromLcaTableData = (attribute: keyof LcaTableData): (string| number)[] => {
-      const values = data.map((d) => d[attribute]).filter((v) => v !== undefined);
+      const values = data.map((d) => d[attribute].toString().trim()).filter((v) => v !== undefined);
       return [...new Set(values)];
     }
 
     //pulls for one specific attribute, in this case its the job title
     columns.map((column, index) => {
-      if (column.filterMode === 'tree') {
+      if (column.filterSearch === true) {
         column.filters = getUniqueValuesFromLcaTableData(column.key as keyof LcaTableData).map((value) => ({ text: value , value: value }));
       }
 
