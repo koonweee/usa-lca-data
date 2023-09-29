429cde0d52c62d96bf44da679c8d6c0b22c86c1a
genechua <genechua@bytedance.com>
2023-09-29 14:53:30 +0800

    add filters bar to desktop view

--- src/components/lca-table/index.tsx
+++ src/components/lca-table/index.tsx
@@ -3,18 +3,30 @@ import { LCADataToTableDataSource } from "@/components/lca-table/formatters";
 import { Layout, Table } from "antd";
 
 import { LCAData } from "@/types/lca";
+import { FilterModal } from "../lca-table-mobile/components/filter-modal";
+import { useState } from "react";
+import { FiltersConfig, useFiltersBar } from "../lca-table-mobile/hooks/use-filters-bar";
+import { FiltersBar } from "@/components/filters-bar";
 interface Props {
   lcaData: LCAData[];
 }
 
-
 export function LCATable({ lcaData }: Props) {
+  const [filterModalConfig, setFilterModalConfig] = useState<FiltersConfig | undefined>(undefined);
+  const filtersConfig = useFiltersBar({ setFilterModalConfig });
 
   // format lcaData as a dataSource
   const lcaDataSource = LCADataToTableDataSource(lcaData);
 
   return (
     <Layout>
+      {filterModalConfig && (
+        <FilterModal
+        filterConfig={filterModalConfig}
+        onCancel={() => setFilterModalConfig(undefined)}
+        />
+      )}
+      <FiltersBar filtersConfig={filtersConfig}/>
       <Table dataSource={lcaDataSource} columns={BASE_LCA_TABLE_COLUMNS} />
     </Layout>
   )
