236fb053678b2724ec9a4f7cd2b6f015723b18c5
Jeremy Tan <jeremy.tan@newfront.com>
2024-06-09 18:00:24 +0800

    remove all

--- src/lib/trpc.ts
+++
@@ -1,5 +0,0 @@
-import type { ServerRouter } from "@/server/router";
-import { createReactQueryHooks } from "@trpc/react";
-
-// trpc hook to make API calls
-export const trpc = createReactQueryHooks<ServerRouter>();
