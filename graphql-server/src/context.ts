4b088d686b478898f257e394e2135234f639213f
Jeremy Tan <jeremy.tan@newfront.com>
2024-06-18 17:28:31 +0800

    update logging

--- graphql-server/src/context.ts
+++ graphql-server/src/context.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 export const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-  ],
+  log: ["query", "info", "warn", "error"],
 });
 
 export interface Context {
@@ -16,13 +11,3 @@ export interface Context {
 export const context: Context = {
   prisma,
 };
-
-prisma.$on("query", async (e) => {
-  const write = `query: ${e.query}\nparams: ${e.params}\nduration: ${e.duration}ms\n`;
-  // Append to log.json
-  fs.appendFile("log.json", write, (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-});
--- react-client/src/main.tsx
+++ react-client/src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: "http://167.71.223.52:4000/:4000",
   cache: new InMemoryCache(),
-})
+});
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
