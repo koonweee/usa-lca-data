664e95b44e415c4a888462375dd9cc58e6dfdda3
Jeremy Tan <jeremy.tan@newfront.com>
2024-06-19 20:15:51 +0800

    cleanup

--- react-client/src/main.tsx
+++ react-client/src/main.tsx
@@ -5,7 +5,11 @@ import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://graphql.h1b1.work",
+  // if NODE_ENV is production, use "https://graphql.h1b1.work", else use "http://localhost:4000"
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://graphql.h1b1.work"
+      : "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
