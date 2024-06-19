8aee84cd8bcb8570a95e917f8a684e3144cff3d6
Jeremy Tan <jeremy.tan@newfront.com>
2024-06-19 18:17:45 +0800

    try internal network

--- react-client/src/main.tsx
+++ react-client/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://167.71.223.52:4000",
+  uri: "http://lca-data-graphql-server:4000",
   cache: new InMemoryCache(),
 });
 
