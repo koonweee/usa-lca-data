7d7c78c0f8e254a9f9e1ad416e38ee31b5a3fd44
Jeremy <jeremytkw98@gmail.com>
2024-06-22 01:26:46 -0700

    cache 6h

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,15 +1,26 @@
 import { schema } from "./schema";
 import { context } from "./context";
-import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginCacheControl } from "@apollo/server/plugin/cacheControl";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 async function startApolloServer() {
   const server = new ApolloServer({
     schema,
-    context
-  })
-  server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`)
+    plugins: [
+      ApolloServerPluginCacheControl({
+        // Cache everything for 1 second by default.
+        defaultMaxAge: 3600 * 6, // 6 hours
+        // Don't send the `cache-control` response header.
+        calculateHttpHeaders: true,
+      }),
+    ],
   });
+  const { url } = await startStandaloneServer(server, {
+    context: async () => context,
+    listen: { port: 4000 },
+  });
+  console.log(`🚀 Server ready at ${url}`);
 }
 
-startApolloServer()
+startApolloServer();
