adf8485de824587c3b1f16233c9d8591b8b1005e
Jeremy <jtkw@Jeremys-MacBook-Air.local>
2024-06-22 00:30:01 -0700

    vercel speed

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -2,11 +2,13 @@ import Layout from "@/components/layout/layout";
 import { ThemeProvider } from "@/components/theme-provider";
 import LCADisclosuresPage from "@/features/disclosures/page";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <Layout>
         <Analytics />
+        <SpeedInsights />
         <LCADisclosuresPage />
       </Layout>
     </ThemeProvider>
