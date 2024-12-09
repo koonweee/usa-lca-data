512a19879f595629bda160ebd6c0fb33a0194574
Jeremy Tan <jeremy.tan@newfront.com>
2024-12-09 00:37:00 -0800

    remove vercel insightys

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,12 +1,10 @@
 import Layout from "@/components/layout/layout";
 import { ThemeProvider } from "@/components/theme-provider";
 import LCADisclosuresPage from "@/features/disclosures/page";
-import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <Layout>
-        <Analytics />
         <LCADisclosuresPage />
       </Layout>
     </ThemeProvider>
